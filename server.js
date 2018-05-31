b346c92679839174ba01263c0b1247385b96d455
gorbasho <37026147+gorbasho@users.noreply.github.com>
2018-05-31 16:59:49 -0500

    sequelize 1.0

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+var db = require("./models");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -20,13 +22,13 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+// Import routes and give the server access to them.
+require("./controllers/burgers_controller.js")(app);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync({}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
